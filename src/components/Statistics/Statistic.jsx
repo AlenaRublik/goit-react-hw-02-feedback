4038718bca23055b2d9c4624736c6bac1ce01e2f
AlenaRublik <114560252+AlenaRublik@users.noreply.github.com>
2023-05-13 02:05:06 +0300

    adding react icon

--- src/components/Statistics/Statistic.jsx
+++ src/components/Statistics/Statistic.jsx
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
+import { TbMoodSmileBeam } from "react-icons/tb";
+import { CgSmileNeutral, CgSmileSad } from "react-icons/cg";
+
 function Statistics({ good, neutral, bad, total, positivePercentage }) {
   return (
     <div>
       <p>
-        Good: <span>{good}</span>
+        <TbMoodSmileBeam /> <span>{good}</span>
       </p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
+      <p><CgSmileNeutral /> {neutral}</p>
+      <p><CgSmileSad /> {bad}</p>
       <p>Total: {total}</p>
       <p>Positive: {positivePercentage}%</p>
     </div>
